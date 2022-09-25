23ce3ea771960245fd0daea0703a6f4aea62bcfc
PPetchiMuthu <112953167+PPetchiMuthu@users.noreply.github.com>
2022-09-25 13:00:03 +0530

    Update IO.java

--- IO.java
+++ IO.java
@@ -1,11 +1,17 @@
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class IO {
     public void readMyFile() throws IOException {
-        FileReader read = new FileReader("IO.java");
-        FileWriter write = new FileWriter("java.txt");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Read from ");
+        String file1 = scan.nextLine();
+        FileReader read = new FileReader(file1);
+        System.out.print("Write in ");
+        String file2 = scan.nextLine();
+        FileWriter write = new FileWriter(file2);
         int lineCount = 1, charCount = 0, n;
         while ((n = read.read()) != -1) {
             write.write((char) n);
