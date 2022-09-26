f97a5e154c84472b5adb7ac586592d7adb993cba
PPetchiMuthu <112953167+PPetchiMuthu@users.noreply.github.com>
2022-09-26 09:40:57 +0530

    Update IO.java

--- IO.java
+++ IO.java
@@ -4,29 +4,34 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class IO {
-    public void readMyFile() throws IOException {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Read from ");
-        String file1 = scan.nextLine();
-        FileReader read = new FileReader(file1);
-        System.out.print("Write in ");
-        String file2 = scan.nextLine();
-        FileWriter write = new FileWriter(file2);
-        int lineCount = 1, charCount = 0, n;
-        while ((n = read.read()) != -1) {
-            write.write((char) n);
-            write.flush();
-            if (n == '\n') lineCount++;
-            if (n != ' ') charCount++;
+    public void readMyFile() {
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Read from ");
+            String file1 = scan.nextLine();
+            FileReader read = new FileReader(file1);
+            System.out.print("Write in ");
+            String file2 = scan.nextLine();
+            FileWriter write = new FileWriter(file2);
+            int lineCount = 1, charCount = 0, n;
+            while ((n = read.read()) != -1) {
+                write.write((char) n);
+                write.flush();
+                if (n == '\n') lineCount++;
+                if (n != ' ') charCount++;
+            }
+            System.out.println("File copy successful");
+            System.out.println("No of lines in a file : " + lineCount);
+            System.out.println("No of characters in a file : " + charCount);
+            System.out.println("File path : " + read);
+            read.close();
+            write.close();
+        } catch (IOException e) {
+            System.out.println("File was not found");
         }
-        System.out.println("No of lines in a file : " + lineCount);
-        System.out.println("No of characters in a file : " + charCount);
-        System.out.println("File path : " + read);
-        read.close();
-        write.close();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         IO obj = new IO();
         obj.readMyFile();
     }
